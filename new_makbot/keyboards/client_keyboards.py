1b79b50067b635a2a2fc12a13afe709a0ce39838
Alexey Markin <leha3300@mail.ru>
2022-06-18 19:33:01 +0300

    new_mak_bot bugfix: multiply send photos now fixed

--- new_makbot/keyboards/client_keyboards.py
+++ new_makbot/keyboards/client_keyboards.py
@@ -1,7 +1,13 @@
 from aiogram import types
 
 
-def inline_pay_buton(tg_id):
+def inline_start():
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(text='Вытянуть карту', callback_data='card'))
+    return markup
+
+
+def inline_pay_button(tg_id):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton(text='490 руб', url='https://snebaupal.ru/makbot/purchase/{}'.format(tg_id)))
     return markup
